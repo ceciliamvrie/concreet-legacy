b0a94eac5da766c6c05b4fd9c6808adc2cd4b514
ceciliamvrie <cecilia.mvrie@gmail.com>
2017-08-15 16:15:47 -0500

    made a modal popdown when cal slot is selected

---
+++ client/components/CreateDateModal.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import Modal from 'react-modal';
+import AddEvent from './AddEvent.jsx';
+// Modal styling
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : '70%',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)',
+    maxHeight             : '325px', // This sets the max height
+    overflow              : 'scroll', // This tells the modal to scroll
+    border                : '1px solid black',
+    //borderBottom          : '1px solid black', // for some reason the bottom border was being cut off, so made it a little thicker
+    borderRadius         : '0px'
+  }
+};
+
+class FreeTimeSlotsModal extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      modalIsOpen: true
+    };
+
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    this.subtitle.style.color = '#f00';
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+
+  render() {
+    return (
+      <div className="timeModal">
+
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Time Slots Modal"
+        >
+
+          <h2 className="modalTitle">Schedule A Date</h2>
+          <AddEvent />
+          {
+            console.log(this.props)
+          }
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default FreeTimeSlotsModal;
